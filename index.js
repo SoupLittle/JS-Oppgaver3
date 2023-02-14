eff8812ba799d194522aeb5ee12bcd233be21866
SoupLittle <112196752+SoupLittle@users.noreply.github.com>
2023-02-14 10:47:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+//Oppgave 1
+
+// let i = 0;
+
+// while (i <= 50) {
+//   console.log(i);
+//   i++;
+// }
+
+//Oppgave 2
+
+// let i = 0;
+
+// while (i <= 100) {
+//   console.log(i);
+//   i+=2;
+// }
+
+//Oppgave 3
+
+// let i = 0;
+
+// while (i <= 999) {
+//   console.log(i);
+//   i+=3;
+// }
+
+//Oppgave 4
+
+// let i = 0;
+
+// while (i <= 1000) {
+//   console.log(i);
+//   i+=15;
+// }
+
+//Oppgave 5
+
+// let i,  sum = 0
+// i = 1
+// while (i<=100){
+//   sum += i;
+//   i++
+// }
+// console.log(sum);
+
+//Oppgave 6
+
+// let i = 1;
+// let text = "";
+
+// while (i <= 15) {
+//     text += "#";
+//     console.log(text);
+//     i += 1;
+// }
+
+//Oppgave 7
+
+let i = 1;
+let text = "";
+
+while (i <= 50) {
+    text += "#";
+    console.log(text);
+    i += 1;
+}
